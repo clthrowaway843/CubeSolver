81593b92677204ec730668e801aef2fd8e50258b
clthrowaway843 <clthrowaway843@gmail.com>
2025-05-12 03:44:41 -0600

    Updates

--- assets/search-dynamic-sgs-unofficial-2TYKOUM4-C0srV_8M.js
+++ assets/search-dynamic-sgs-unofficial-2TYKOUM4-C0srV_8M.js
@@ -1,4 +1,4 @@
-import{p as H}from"./chunk-Y3BVWVFU-CZjqCxXa.js";import{A as N,v as E,R as y}from"./index-DmQZk0vk.js";var J=null;async function pB(){return J??(J=K())}async function K(){return H(await E.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
+import{p as H}from"./chunk-Y3BVWVFU-CaQKhX4h.js";import{A as N,v as E,R as y}from"./index-D_oWrq5Q.js";var J=null;async function pB(){return J??(J=K())}async function K(){return H(await E.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
 
 Alg T
 Alg B
